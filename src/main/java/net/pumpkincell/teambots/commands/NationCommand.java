fee34227984b54914f3d70ae428369fb5e4b010a
Maciej <sppmacd@pm.me>
2023-04-08 23:31:41 +0200

    Implement cooldown for /nation command
    Uses an arbitrary value of 5 minutes, works for all nation commands
    except list and admin.

--- src/main/java/net/pumpkincell/teambots/commands/NationCommand.java
+++ src/main/java/net/pumpkincell/teambots/commands/NationCommand.java
@@ -12,24 +12,43 @@ import net.minecraft.command.argument.TeamArgumentType;
 import net.minecraft.scoreboard.Team;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.pumpkincell.teambots.ServerState;
 import net.pumpkincell.teambots.TeamBotsMod;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 public class NationCommand {
+    public static final Map<UUID, Long> lastNationCommandRunTimes = new HashMap<>();
+
+    private static void handleCooldown(ServerPlayerEntity player) {
+        var currentTime = System.currentTimeMillis();
+        var lastRunTime = lastNationCommandRunTimes.getOrDefault(player.getUuid(), 0L);
+        final int COMMAND_COOLDOWN = 1000 * 60 * 5; // 5 minutes, in ms
+        if (currentTime < lastRunTime + COMMAND_COOLDOWN) {
+            throw new CommandException(Text.literal(String.format("You can use this command in %d second(s) next time",
+                ((lastRunTime + COMMAND_COOLDOWN) - currentTime) / 1000 + 1)));
+        }
+        lastNationCommandRunTimes.put(player.getUuid(), currentTime);
+    }
+
     private static int handleNationLeave(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
         var source = context.getSource();
         var player = source.getPlayer();
-
+        assert player != null;
+        handleCooldown(player);
         TeamBotsMod.requireIsInNation(source);
 
-        assert player != null;
         var state = ServerState.load(source.getServer());
-        if (state.isLeader(source.getServer(), (Team) Objects.requireNonNull(player.getScoreboardTeam()), player.getGameProfile().getId())) {
+        if (state.isLeader(source.getServer(), (Team) Objects.requireNonNull(player.getScoreboardTeam()),
+            player.getGameProfile()
+            .getId())) {
             throw new CommandException(Text.literal("You can't leave nation if you are its leader"));
         }
 
@@ -41,11 +60,12 @@ public class NationCommand {
     private static int handleNationRemove(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
         var source = context.getSource();
         var player = source.getPlayer();
+        assert player != null;
+        handleCooldown(player);
 
         TeamBotsMod.requireIsNationLeader(source);
 
-        assert player != null;
-        var team = (Team)player.getScoreboardTeam();
+        var team = (Team) player.getScoreboardTeam();
         assert team != null;
 
         if (team.getPlayerList().size() > 1) {
@@ -64,10 +84,11 @@ public class NationCommand {
         var color = ColorArgumentType.getColor(context, "color");
         var source = context.getSource();
         var player = source.getPlayer();
+        assert player != null;
+        handleCooldown(player);
 
         TeamBotsMod.requireIsNotInNation(source);
 
-        assert player != null;
         if (player.getScoreboardTeam() != null) {
             throw new CommandException(Text.literal("You are already in a nation! If you want to leave, use /nation " +
                 "leave first"));
@@ -93,10 +114,11 @@ public class NationCommand {
         var source = context.getSource();
         var player = source.getPlayer();
         assert player != null;
+        handleCooldown(player);
 
         TeamBotsMod.requireIsNationLeader(source);
 
-        var team = (Team)player.getScoreboardTeam();
+        var team = (Team) player.getScoreboardTeam();
         assert team != null;
 
         var playerCountInTeamAfterAdding = team.getPlayerList().size() + playersToAdd.size();
@@ -121,20 +143,25 @@ public class NationCommand {
                     .formatted(Formatting.BOLD)).append(" to your nation"), false);
         } else {
             source.sendFeedback(Text.literal(String.format("Successfully added %d players to your nation",
-                playersToAdd.size())), false);
+                playersToAdd.size()
+            )), false);
         }
-        for (var teammate: team.getPlayerList()) {
+        for (var teammate : team.getPlayerList()) {
             for (var addedPlayer : playersToAdd) {
                 var teammateEntity = source.getServer().getPlayerManager().getPlayer(teammate);
                 var addedPlayerEntity = source.getServer().getPlayerManager().getPlayer(addedPlayer.getId());
-                var addedPlayerName = addedPlayerEntity == null ? Text.literal(addedPlayer.getName()) : addedPlayerEntity.getDisplayName();
+                var addedPlayerName = addedPlayerEntity == null ? Text.literal(addedPlayer.getName()) :
+                    addedPlayerEntity.getDisplayName();
                 if (teammateEntity != null) {
                     if (playersToAdd.contains(teammateEntity.getGameProfile())) {
-                        teammateEntity.sendMessageToClient(Text.literal(String.format("∙ You have been added to %s.", team.getName())).formatted(Formatting.ITALIC), false);
+                        teammateEntity.sendMessageToClient(Text.literal(String.format("∙ You have been added to %s.",
+                                team.getName()))
+                            .formatted(Formatting.ITALIC), false);
                     } else {
                         teammateEntity.sendMessageToClient(Text.literal("∙ ").formatted(Formatting.ITALIC)
                             .append(addedPlayerName.copy())
-                            .append(Text.literal(" has joined our nation. Welcome!").formatted(Formatting.RESET, Formatting.ITALIC)), false);
+                            .append(Text.literal(" has joined our nation. Welcome!")
+                                .formatted(Formatting.RESET, Formatting.ITALIC)), false);
                     }
                 }
             }
@@ -142,15 +169,17 @@ public class NationCommand {
         return 0;
     }
 
-    private static int handleNationKick(CommandContext<ServerCommandSource> context, String reason) throws CommandSyntaxException {
+    private static int handleNationKick(CommandContext<ServerCommandSource> context, String reason)
+        throws CommandSyntaxException {
         var playersToKick = GameProfileArgumentType.getProfileArgument(context, "player");
         var source = context.getSource();
         var player = source.getPlayer();
+        assert player != null;
+        handleCooldown(player);
 
         TeamBotsMod.requireIsNationLeader(source);
 
-        assert player != null;
-        var team = (Team)player.getScoreboardTeam();
+        var team = (Team) player.getScoreboardTeam();
 
         assert team != null;
         for (var playerToKick : playersToKick) {
@@ -171,21 +200,27 @@ public class NationCommand {
                     .formatted(Formatting.BOLD)).append(" from your nation"), false);
         } else {
             source.sendFeedback(Text.literal(String.format("Successfully kicked %d players from your nation",
-                playersToKick.size())), false);
+                playersToKick.size()
+            )), false);
         }
         for (var kickedPlayer : playersToKick) {
             var kickedPlayerEntity = source.getServer().getPlayerManager().getPlayer(kickedPlayer.getId());
-            for (var teammate: team.getPlayerList()) {
+            for (var teammate : team.getPlayerList()) {
                 var teammateEntity = source.getServer().getPlayerManager().getPlayer(teammate);
-                var kickedPlayerName = kickedPlayerEntity == null ? Text.literal(kickedPlayer.getName()) : kickedPlayerEntity.getDisplayName();
+                var kickedPlayerName = kickedPlayerEntity == null ? Text.literal(kickedPlayer.getName()) :
+                    kickedPlayerEntity.getDisplayName();
                 if (teammateEntity != null) {
                     teammateEntity.sendMessageToClient(Text.literal("∙ ").formatted(Formatting.ITALIC)
                         .append(kickedPlayerName.copy())
-                        .append(Text.literal(String.format(" has been kicked from our nation. Reason: %s", reason))), false);
+                        .append(Text.literal(String.format(" has been kicked from our nation. Reason: %s", reason))),
+                        false);
                 }
             }
-            kickedPlayerEntity.sendMessageToClient(Text.literal(String.format("∙ You have been kicked from %s. Reason: %s",
-                team.getName(), reason)).formatted(Formatting.ITALIC), false);
+            assert kickedPlayerEntity != null;
+            kickedPlayerEntity.sendMessageToClient(Text.literal(String.format("∙ You have been kicked from %s. " +
+                    "Reason: %s",
+                team.getName(), reason
+            )).formatted(Formatting.ITALIC), false);
         }
         return 0;
     }
@@ -217,7 +252,8 @@ public class NationCommand {
                 } else {
                     text.append(Text.literal("(Invalid leader)").formatted(Formatting.BOLD, Formatting.RED));
                 }
-            } var playersInTeam = team.getPlayerList().stream().toList();
+            }
+            var playersInTeam = team.getPlayerList().stream().toList();
             for (String playerInTeam : playersInTeam) {
                 var profile = server.getUserCache().findByName(playerInTeam);
                 if (profile.isEmpty()) {
@@ -235,7 +271,8 @@ public class NationCommand {
         return 0;
     }
 
-    private static int handleNationAdminSetLeader(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+    private static int handleNationAdminSetLeader(CommandContext<ServerCommandSource> context)
+        throws CommandSyntaxException {
         var team = StringArgumentType.getString(context, "team");
         var leaders = GameProfileArgumentType.getProfileArgument(context, "leader");
         if (leaders.size() != 1) {
@@ -252,7 +289,8 @@ public class NationCommand {
         var newLeader = newLeaderO.get();
         state.setLeader(team, newLeader.getId());
         source.sendFeedback(Text.literal(String.format("Successfully set player %s as leader of %s",
-            newLeader.getName(), team)), true);
+            newLeader.getName(), team
+        )), true);
         return 0;
     }
 
@@ -274,47 +312,48 @@ public class NationCommand {
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("nation")
-            .requires(ServerCommandSource::isExecutedByPlayer)
-            .then(CommandManager.literal("leave")
-                .executes(NationCommand::handleNationLeave))
-            .then(CommandManager.literal("remove")
-                .executes(NationCommand::handleNationRemove))
-            .then(CommandManager.literal("create")
-                .then(CommandManager.argument("name", StringArgumentType.string())
-                    .then(CommandManager.argument("color", ColorArgumentType.color())
-                        .executes(NationCommand::handleNationCreate)
-                    )
-                )
-            )
-            .then(CommandManager.literal("add")
-                .then(CommandManager.argument("player", GameProfileArgumentType.gameProfile())
-                    .executes(NationCommand::handleNationAdd)
-                )
-            )
-            .then(CommandManager.literal("kick")
-                .then(CommandManager.argument("player", GameProfileArgumentType.gameProfile())
-                    .executes((context) -> handleNationKick(context, "Just because"))
-                    .then(CommandManager.argument("reason", StringArgumentType.greedyString())
-                        .executes((context) -> handleNationKick(context, StringArgumentType.getString(context, "reason")))
-                    )
-                )
-            )
-            .then(CommandManager.literal("list").executes(NationCommand::handleNationList))
-            .then(CommandManager.literal("admin")
-                .requires(source -> source.hasPermissionLevel(3))
-                .then(CommandManager.literal("setleader")
-                    .then(CommandManager.argument("team", TeamArgumentType.team())
-                        .then(CommandManager.argument("leader", GameProfileArgumentType.gameProfile())
-                            .executes(NationCommand::handleNationAdminSetLeader)
+                .requires(ServerCommandSource::isExecutedByPlayer)
+                .then(CommandManager.literal("leave")
+                    .executes(NationCommand::handleNationLeave))
+                .then(CommandManager.literal("remove")
+                    .executes(NationCommand::handleNationRemove))
+                .then(CommandManager.literal("create")
+                    .then(CommandManager.argument("name", StringArgumentType.string())
+                        .then(CommandManager.argument("color", ColorArgumentType.color())
+                            .executes(NationCommand::handleNationCreate)
                         )
                     )
                 )
-                .then(CommandManager.literal("clearleader")
-                    .then(CommandManager.argument("team", TeamArgumentType.team())
-                        .executes(NationCommand::handleNationAdminClearLeader)
+                .then(CommandManager.literal("add")
+                    .then(CommandManager.argument("player", GameProfileArgumentType.gameProfile())
+                        .executes(NationCommand::handleNationAdd)
                     )
                 )
-            )
+                .then(CommandManager.literal("kick")
+                    .then(CommandManager.argument("player", GameProfileArgumentType.gameProfile())
+                        .executes((context) -> handleNationKick(context, "Just because"))
+                        .then(CommandManager.argument("reason", StringArgumentType.greedyString())
+                            .executes((context) -> handleNationKick(context, StringArgumentType.getString(context,
+                                "reason")))
+                        )
+                    )
+                )
+                .then(CommandManager.literal("list").executes(NationCommand::handleNationList))
+                .then(CommandManager.literal("admin")
+                    .requires(source -> source.hasPermissionLevel(3))
+                    .then(CommandManager.literal("setleader")
+                        .then(CommandManager.argument("team", TeamArgumentType.team())
+                            .then(CommandManager.argument("leader", GameProfileArgumentType.gameProfile())
+                                .executes(NationCommand::handleNationAdminSetLeader)
+                            )
+                        )
+                    )
+                    .then(CommandManager.literal("clearleader")
+                        .then(CommandManager.argument("team", TeamArgumentType.team())
+                            .executes(NationCommand::handleNationAdminClearLeader)
+                        )
+                    )
+                )
             // TODO: /nation kick
         );
     }
